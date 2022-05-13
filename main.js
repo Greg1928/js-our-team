d8190957aa0b931798126c2608aa0e1d7d91c51b
Greg1928 <gregoriosortino@protonmail.com>
2022-05-13 16:52:29 +0200

    added team exercise

---
+++ main.js
@@ -0,0 +1,48 @@
+const team = [
+{
+    name : "Angela Lopez",
+    role : "Social Media Manager",
+    photo : "img/angela-lopez-social-media-manager.jpg",
+},
+{
+    name : "Angela Caroll",
+    role : "Chief Editor",
+    photo : "img/angela-caroll-chief-editor.jpg",
+},
+{
+    name : "Barbara Ramos",
+    role : "Graphic Designer",
+    photo : "img/barbara-ramos-graphic-designer.jpg",
+},
+{
+    name : "Scott Estrada",
+    role : "Developer",
+    photo : "img/scott-estrada-developer.jpg",
+},
+{
+    name : "Walter Gordon",
+    role : "Office Manager",
+    photo : "img/walter-gordon-office-manager.jpg",
+}
+]
+
+const teamContainer = document.querySelector(".team-container");
+
+for(let i = 0; i<team.length; i++){
+    console.log(team[i]);
+    const teamMember = `
+        <div class="team-card">
+        <div class="card-image">
+            <img
+            src="${team[i].photo}"
+            alt="Wayne Barnett"
+            />
+        </div>
+        <div class="card-text">
+            <h3>W${team[i].name}</h3>
+            <p>${team[i].role}</p>
+        </div>
+        </div>
+    `;
+    teamContainer.innerHTML += teamMember;
+}
