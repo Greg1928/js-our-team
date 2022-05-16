321bca884abb7f3023b9f54de530c38d0976283c
Greg1928 <gregoriosortino@protonmail.com>
2022-05-16 12:29:54 +0200

    snack 1

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ for(let i = 0; i<team.length; i++){
             />
         </div>
         <div class="card-text">
-            <h3>W${team[i].name}</h3>
+            <h3>${team[i].name}</h3>
             <p>${team[i].role}</p>
         </div>
         </div>
